eda141c00feb2b2109c016bdf97a62b4343b58e6
Immanuel Dsouza <dsouzad681@gmail.com>
2024-11-17 03:18:39 +0530

    mongodb & redis connection to implement write-back policy

--- PenCraft-Backend/cmd/main.go
+++ PenCraft-Backend/cmd/main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
-	"PencraftB/utils"	
-	db "PencraftB/repository"
-	"time"
+	redisDb "PencraftB/repository"
+	"PencraftB/utils"
+	mongoDb "PencraftB/repository"
+	"net/http"
+	"log"
 )
 
 func main(){
 	utils.Logger("Started main driver function")
 
-	client := db.NewDBClient()
+	client := mongoDb.NewDBClient()
+	rdb := redisDb.GetRedisInstance()
+
+	log.Println("Starting server on :8080")
+
 	defer client.Close();
-	time.Sleep(3 * time.Second)
+	defer rdb.Close();
+
+	
 }
