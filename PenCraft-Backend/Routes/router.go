ba83c5de79af2877cf9b300d58d4c24bfd0ec689
Immanuel Dsouza <dsouzad681@gmail.com>
2024-11-22 23:53:48 +0530

    wip

--- PenCraft-Backend/Routes/router.go
+++ PenCraft-Backend/Routes/router.go
@@ -9,17 +9,17 @@ import (
 func Router() *mux.Router {
 	router := mux.NewRouter()
 
-	router.Handle("/createblog", service.RateLimiter(controller.CreateBlogController)).Methods("POST")
-	router.Handle("/addtag", service.RateLimiter(controller.CreateTagController)).Methods("POST")
+	router.Handle("/api/v1/blog", service.RateLimiter(controller.CreateBlogController)).Methods("POST")
+	router.Handle("/api/v1/tag", service.RateLimiter(controller.CreateTagController)).Methods("POST")
 
-	router.Handle("/getalltags", service.RateLimiter(controller.FetchAllTagController)).Methods("GET")
-	router.Handle("/getallblogs", service.RateLimiter(controller.FetchAllBlogController)).Methods("GET")
+	router.Handle("/api/v1/tags", service.RateLimiter(controller.FetchAllTagController)).Methods("GET")
+	router.Handle("/api/v1/blogs", service.RateLimiter(controller.FetchAllBlogController)).Methods("GET")
 	router.Handle("/blog/{blog_id}",service.RateLimiter(controller.FetchBlogbyBlogIdController)).Methods("GET")
 
-	router.Handle("/updateblog", service.RateLimiter(controller.UpdateBlogController)).Methods("PUT")
+	router.Handle("/blog", service.RateLimiter(controller.UpdateBlogController)).Methods("PUT")
 
-	router.Handle("/hdeleteblog/{blog_id}", service.RateLimiter(controller.HardDeleteBlogbyBlogidController)).Methods("DELETE")
-	router.Handle("/deleteall/danger",service.RateLimiter(controller.DeleteAllDataController)).Methods("DELETE");
+	router.Handle("/blog/{blog_id}", service.RateLimiter(controller.HardDeleteBlogbyBlogidController)).Methods("DELETE")
+	router.Handle("/api/v1/deleteall",service.RateLimiter(controller.DeleteAllDataController)).Methods("DELETE");
 
 
 	return router
